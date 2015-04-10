79625c22cc0a90de2d16e1f49ff25c054d03d10a
Terin Patel-Wilson <terinpw93@gmail.com>
2015-04-10 17:05:12 -0400

    UI for Config Page

---
+++ CrowdSound/CrowdSound/ConfigTableViewController.m
@@ -0,0 +1,13 @@
+//
+//  ConfigTableViewController.m
+//  CrowdSound
+//
+//  Created by Terin Patel-Wilson on 4/10/15.
+//  Copyright (c) 2015 cs439. All rights reserved.
+//
+
+#import "ConfigTableViewController.h"
+
+@implementation ConfigTableViewController
+
+@end
