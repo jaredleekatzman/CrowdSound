687ab1dd0c7cff95897fc37bed900736589d6d45
Terin Patel-Wilson <terinpw93@gmail.com>
2015-04-19 15:51:57 -0400

    added new search bar, error: UI is messed up

--- CrowdSound/CrowdSound/SearchViewController.swift
+++ CrowdSound/CrowdSound/SearchViewController.swift
@@ -8,43 +8,44 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchResultsUpdating {
     var crowd : Crowd?
-    @IBOutlet weak var searchButton: UIButton!
-    @IBOutlet weak var searchField: UITextField!
     
-    @IBAction func searchForSongByString(sender: AnyObject) {
-        let searchString = searchField.text
-        if searchString.isEmpty {
-            return
-        }
-        
-        SPTRequest.performSearchWithQuery(searchString, queryType: SPTSearchQueryType.QueryTypeTrack, offset: 0, session: nil, callback: {(error: NSError!, result:AnyObject!) -> Void in
-            
-            if error != nil {
-                println("error performing query")
-                return
-            }
-            let trackListPage = result as SPTListPage
-            let partialTrack = trackListPage.items.first as SPTPartialTrack
-            // using first result for now.
-            // will be able to use items.count to get the whole list...
-            
-            var newSong = Song()
-            newSong.name = partialTrack.name
-            newSong.spotifyURI = partialTrack.playableUri
-            newSong.upvotes = 1
-            self.crowd?.pending.addSong(newSong)
-            println("Adding a new song")
-            self.searchField.text = ""
-        })
-
+    
+    @IBOutlet weak var songTable: UITableView!
+    
+    var searchArray:[Song] = [Song](){
+        didSet  {self.songTable.reloadData()}
     }
+    var songSearchController = UISearchController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let tbvc = self.tabBarController as CrowdTabViewController
         crowd = tbvc.myCrowd
         // Do any additional setup after loading the view.
+        
+        
+        // Configure countryTable
+        self.songTable.delegate = self
+        self.songTable.dataSource = self
+        
+        
+        // Configure countrySearchController
+        self.songSearchController = ({
+            // Two setups provided below:
+            
+            // Setup One: This setup present the results in the current view.
+            let controller = UISearchController(searchResultsController: nil)
+            controller.searchResultsUpdater = self
+            controller.hidesNavigationBarDuringPresentation = false
+            controller.dimsBackgroundDuringPresentation = false
+            controller.searchBar.searchBarStyle = .Minimal
+            controller.searchBar.sizeToFit()
+            self.songTable.tableHeaderView = controller.searchBar
+            
+            return controller
+        })()
     }
 
     override func didReceiveMemoryWarning() {
@@ -52,6 +53,10 @@ class SearchViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+        self.songTable.reloadData()
+    }
 
     /*
     // MARK: - Navigation
@@ -62,5 +67,90 @@ class SearchViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        if (self.songSearchController.active)
+        {
+            return self.searchArray.count
+            
+        } else
+        {
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        var cell = self.songTable.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell
+        
+        if (self.songSearchController.active)
+        {
+            cell.textLabel?.text = self.searchArray[indexPath.row].name
+            return cell
+        }
+            
+        else
+        {
+            // TODO: worried about an error in this branch.
+            //cell.textLabel?.text! = self.countryArray[indexPath.row]
+            return cell
+        }
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
+    {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        self.crowd?.pending.addSong(searchArray[indexPath.row])
+    }
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController)
+    {
+        self.searchArray.removeAll(keepCapacity: false)
+        
+        // let searchPredicate = NSPredicate(format:ELF CONTAINS[c] %@", searchController.searchBar.text)
+        // let array = (self.countryArray as NSArray).filteredArrayUsingPredicate(searchPredicate)
+        // self.searchArray = array as! [String]
+        
+        
+        let searchString = searchController.searchBar.text
+        
+        SPTRequest.performSearchWithQuery(searchString, queryType: SPTSearchQueryType.QueryTypeTrack, offset: 0, session: nil, callback: {(error: NSError!, result:AnyObject!) -> Void in
+            
+            if error != nil {
+                println("error performing query")
+                return
+            }
+            let trackListPage = result as SPTListPage
+            //let partialTrack = trackListPage.items.first as SPTPartialTrack
+            // using first result for now.
+            // will be able to use items.count to get the whole list...
+            
+            let items = trackListPage.items
+
+            var resultsArray = [Song]()
+            
+            for item in items {
+                var newSong = Song()
+                let partialTrack = item as SPTPartialTrack
+                newSong.name = partialTrack.name
+                println(newSong.name)
+                newSong.spotifyURI = partialTrack.playableUri
+                newSong.upvotes = 1
+                // TODO: add the artist name to the song!!
+                resultsArray.append(newSong)
+//                self.searchArray.append(newSong)
+            }
+            println("Results Array: \(resultsArray.map { a in a.name })")
+            
+            self.searchArray = resultsArray
+            println("Search Array: \(self.searchArray.map { a in a.name })")
+
+//            self.crowd?.pending.addSong(newSong)
+//            println("Adding a new song")
+//            self.searchField.text = ""
+        })
+    }
 
 }
