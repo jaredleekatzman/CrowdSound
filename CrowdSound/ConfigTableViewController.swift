c6d66e4492ee5f96ede6c1b65f5992f51aadbecb
Terin Patel-Wilson <terinpw93@gmail.com>
2015-04-21 03:24:11 -0400

    moved login button to configuration page - now users only log in to create new crowds. TODO: make users log in to spotify when joining crowd they are hosts for (if session is now nil).

--- CrowdSound/ConfigTableViewController.swift
+++ CrowdSound/ConfigTableViewController.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class ConfigTableViewController: UITableViewController {
-
+class ConfigTableViewController: UITableViewController, SPTAuthViewDelegate {
+    
     @IBOutlet weak var crowdNameText: UITextField!
     @IBOutlet weak var hostNameText: UITextField!
     @IBOutlet weak var upvotesSlider: UISlider!
@@ -18,6 +18,116 @@ class ConfigTableViewController: UITableViewController {
     @IBOutlet weak var passwordText: UITextField!
     @IBOutlet weak var confirmPasswordText: UITextField!
     @IBOutlet weak var passwordProtectedSwitch: UISwitch!
+    @IBOutlet weak var spotifyStatusLabel: UILabel!
+    @IBOutlet weak var spotifyLoginBtn: UIButton!
+
+    var authViewController : SPTAuthViewController?
+    
+    
+    override func viewDidLoad() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "sessionUpdateNotification:", name: "sessionUpdated", object: nil)
+        if isLoggedIntoSpotify() {
+            spotifyStatusLabel.text = "Spotify Status: Logged In"
+            spotifyLoginBtn.setTitle("Log Out", forState: UIControlState.Normal)
+        }
+    }
+    
+    func sessionUpdatedNotification(notification : NSNotification) {
+        if self.navigationController?.topViewController == self {
+            let auth = SPTAuth.defaultInstance()
+            if (auth.session != nil && auth.session.isValid()) {
+                
+                // TODO: change so button is now logout or somethign
+            }
+        }
+    }
+    
+    func isLoggedIntoSpotify() -> Bool {
+        let auth = SPTAuth.defaultInstance()
+        if auth.session != nil && auth.session.isValid() {
+            return true
+        }
+        return false
+    }
+    
+    @IBAction func spotifyBtnPressed(sender: AnyObject) {
+        let auth = SPTAuth.defaultInstance()
+        if isLoggedIntoSpotify() {
+            auth.session = nil
+            spotifyStatusLabel.text = "Spotify Status: Logged Out"
+            spotifyLoginBtn.setTitle("Log In", forState: UIControlState.Normal)
+        } else {
+            self.openLoginPage()
+        }
+    }
+    
+    func authenticationViewController(authenticationViewController: SPTAuthViewController!, didFailToLogin error: NSError!) {
+        NSLog("*** Failed to log in \(error)")
+    }
+    
+    func authenticationViewController(authenticationViewController: SPTAuthViewController!, didLoginWithSession session: SPTSession!) {
+        // Login worked!
+        spotifyStatusLabel.text = "Spotify Status: Logged In"
+        spotifyLoginBtn.setTitle("Log Out", forState: UIControlState.Normal)
+        
+    }
+    
+    func authenticationViewControllerDidCancelLogin(authenticationViewController: SPTAuthViewController!) {
+        // Login Cancelled
+    }
+    
+    func openLoginPage() {
+        self.authViewController = SPTAuthViewController.authenticationViewController()
+        self.authViewController?.delegate = self;
+        self.authViewController?.modalPresentationStyle = UIModalPresentationStyle.CurrentContext
+        self.authViewController?.modalTransitionStyle = UIModalTransitionStyle.CrossDissolve
+        
+        self.modalPresentationStyle = UIModalPresentationStyle.CurrentContext
+        self.definesPresentationContext = true;
+        
+        self.presentViewController(self.authViewController!, animated: false, completion: nil)
+    }
+    
+    func renewTokenAndShowButton() {
+        let auth = SPTAuth.defaultInstance()
+        
+        auth.renewSession(auth.session, callback: { (error : NSError?, session : SPTSession?) -> () in
+            auth.session = session
+            
+            if (error != nil) {
+                NSLog("*** Error renewing session \(error)")
+                return
+            }
+        })
+    }
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        var auth = SPTAuth.defaultInstance()
+        
+        // Check if we have a token at all
+        if auth.session == nil {
+            NSLog("Have a token")
+            return
+        }
+        
+        // Check if it's still valid
+        if auth.session.isValid() {
+            
+            NSLog("session is valid")
+            // Change buttons
+            return
+        }
+        
+        if auth.hasTokenRefreshService {
+            NSLog("renew Token!")
+            
+            self.renewTokenAndShowButton()
+            return
+        }
+        
+    }
+    
     
     @IBAction func upvoteSliderMoved(sender: AnyObject) {
         var sliderVal = Int(upvotesSlider.value)
@@ -34,6 +144,9 @@ class ConfigTableViewController: UITableViewController {
         }
         if (hostNameText.text.isEmpty) {
             alertMsg += "Host must have a name. "
+        }
+        if (!isLoggedIntoSpotify()) {
+            alertMsg += "You must log into spotify. "
         }
         
         // requirements if password protected
@@ -60,7 +173,7 @@ class ConfigTableViewController: UITableViewController {
             secondScene.myCrowd = createCrowd()
             
             // TODO: add crowd to database
-            // TODO: figure out how not to go all the way back to crowd preferences!! 
+            // TODO: figure out how not to go all the way back to crowd preferences!!
         }
     }
     
