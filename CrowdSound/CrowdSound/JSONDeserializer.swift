402d6539a8e29622001d90b0efce123c0e0d9f8d
Terin Patel-Wilson <terinpw93@gmail.com>
2015-04-29 02:05:58 -0400

    begin deserialization

---
+++ CrowdSound/CrowdSound/JSONDeserializer.swift
@@ -0,0 +1,24 @@
+//
+//  JSONDeserializer.swift
+//  CrowdSound
+//
+//  Created by Terin Patel-Wilson on 4/29/15.
+//  Copyright (c) 2015 cs439. All rights reserved.
+//
+
+import Foundation
+
+class JSONDeserializer {
+    
+    class func deserializePending() {
+    
+    }
+    
+    class func deserializePlaylist(){
+        
+    }
+    
+    class func deserializeCrowdsList() {
+        
+    }
+}
