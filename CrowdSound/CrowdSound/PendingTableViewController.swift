3762ecdf2672fee748c256966574715cd286266b
Terin Patel-Wilson <terinpw93@gmail.com>
2015-04-29 05:10:59 -0400

    localhost instead of server

--- CrowdSound/CrowdSound/PendingTableViewController.swift
+++ CrowdSound/CrowdSound/PendingTableViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class PendingTableViewController: UITableViewController {
     
     // define socket.io in class
-    let socket = SocketIOClient(socketURL: "54.152.114.46/")
+    let socket = SocketIOClient(socketURL: "localhost:8080")
     
     // Crowd Data
     var crowd : Crowd?
