905c9897540ba5cd374c727559cf07a161e0f286
Terin Patel-Wilson <terinpw93@gmail.com>
2015-04-29 06:58:10 -0400

    fixed bug spotifyURI being string not NSURL

--- CrowdSound/CrowdSound/Song.swift
+++ CrowdSound/CrowdSound/Song.swift
@@ -29,7 +29,7 @@ class Song {
     init(name : String, artist : String, uri: NSURL) {
         self.name = name
         self.artist = artist
-        self.spotifyURI = uri
+        self.spotifyURI = NSURL(string: uri)
         self.upvotes = 1
         self.uid = name + artist + NSUUID().UUIDString
         self.spotifyAlbumArtURL = NSURL(string: "nil")!
